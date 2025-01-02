c846d286df1840a9b0b27eeb3962543a86ca8774
Jonas Holfeld <klatschpauke@googlemail.com>
2025-01-02 16:36:34 +0100

    new old home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <h1>{{ site?.title }}</h1>
   </div>
   <div
+    ref="homenavigation"
     class="home-navigation"
     :style="
       '--workcolor: ' +
@@ -29,6 +30,7 @@
           :canMoveLeft="work.canMoveLeft"
           :canMoveRight="work.canMoveRight"
           :class="[
+            index == 0 && transitionFirstElements ? 'transitionleft' : '',
             work.title === 'empty' ? 'empty' : '',
             work.addClass,
             !showWorkElements ? 'hide' : '',
@@ -57,6 +59,7 @@
           :canMoveLeft="info.canMoveLeft"
           :canMoveRight="info.canMoveRight"
           :class="[
+            index == 0 && transitionFirstElements ? 'transitionleft' : '',
             info.title === 'empty' ? 'empty' : '',
             info.addClass,
             !showInfoElements ? 'hide' : '',
@@ -72,7 +75,7 @@
 
 <script setup>
 import { useSite } from "~/composables";
-import { onMounted, onUnmounted, ref, toRaw } from "vue";
+import { onActivated, onMounted, onUnmounted, ref, toRaw } from "vue";
 import { useRouter } from "vue-router";
 import { gsap } from "gsap";
 
@@ -81,11 +84,17 @@ const works = ref([]);
 const infos = ref([]);
 const showWorkElements = ref(false);
 const showInfoElements = ref(false);
+const transitionFirstElements = ref(true);
 const draggableWorks = ref(null);
+const homenavigation = ref(null);
 const draggableInfos = ref(null);
 const initialXWorks = 32 * (window.innerWidth / 100);
 const initialXInfo = 1 * (window.innerWidth / 100);
 const router = useRouter();
+
+onActivated(() => {
+  document.body.classList.remove("dark-body");
+});
 
 const initializeData = async () => {
   try {
@@ -127,7 +136,6 @@ const generateEmptyElement = (initialX, id, label) => {
     if (label == "Arbeiten") {
       return initialX - 5 * vw * (index + 1);
     } else {
-      console.log(initialX);
       return initialX + 5 * vw * (index + 1);
     }
   }
@@ -147,7 +155,6 @@ const generateEmptyElement = (initialX, id, label) => {
 const populateElements = (sourceArray, targetArray, initialX, label, zeroX) => {
   let xOffset;
   if (label == "Arbeiten") {
-    console.log("hi");
     xOffset = initialX;
   }
   targetArray.value[0] = {
@@ -183,23 +190,80 @@ const populateElements = (sourceArray, targetArray, initialX, label, zeroX) => {
   function getLeft(index) {
     const vw = window.innerWidth / 100;
     if (label == "Arbeiten") {
-      return initialX - 5 * vw * (index + 1);
+      return initialX - 5 * vw * (index + 1) + 2;
     } else {
-      console.log(initialX);
       return initialX + 5 * vw * (index + 1);
     }
   }
 };
 
+const resetPlacements = () => {
+  const newInitialXworks = 32 * (window.innerWidth / 100);
+  const newInitialXInfos = 1 * (window.innerWidth / 100);
+  const vw = window.innerWidth / 100;
+  works.value.forEach((work, index) => {
+    if (index == 0) {
+      work.left = newInitialXworks;
+    } else {
+      if (index == 1) {
+        console.log(
+          "left value of first work ",
+          newInitialXworks - 5 * vw * (index + 1)
+        );
+      }
+      work.left = newInitialXworks - 5 * vw * index;
+    }
+  });
+  infos.value.forEach((work, index) => {
+    if (index == 0) {
+      work.left = newInitialXInfos;
+    } else {
+      work.left = newInitialXInfos + 5 * vw * index;
+    }
+  });
+};
+
+const mobileCheck = function () {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
+};
+
 onMounted(() => {
+  if (mobileCheck()) {
+    console.log("mobile!! ");
+    gsap.set(homenavigation.value, { y: -window.innerHeight });
+    gsap.to(homenavigation.value, {
+      y: 0,
+      duration: 1.5,
+      ease: "bounce.out",
+    });
+  }
+  window.addEventListener("resize", () => {
+    resetPlacements();
+  });
   initializeData();
   setTimeout(() => {
     works.value[0].left = initialXWorks;
     infos.value[0].left = initialXInfo;
   }, 500);
+  setTimeout(() => {
+    transitionFirstElements.value = false;
+  }, 2000);
 });
 
 const showElements = (elementArray) => {
+  console.log(draggableWorks.value, draggableInfos.value);
   if (elementArray == "works") {
     if (!showWorkElements.value) {
       showWorkElements.value = true;
@@ -244,7 +308,6 @@ let currentURLtoNavigate;
 let mouseDownTime;
 
 const startDrag = (index, workOrInfo, event, url) => {
-  console.log(url);
   currentURLtoNavigate = url;
   clickBeginning = event.clientX;
   mouseDownTime = Date.now();
@@ -517,7 +580,6 @@ const stopDrag = (event) => {
     Math.abs(event.clientX - clickBeginning) < 3 &&
     mouseDownTime - Date.now() < 300
   ) {
-    console.log("click!!!");
     if (currentURLtoNavigate) {
       router.push("/" + currentURLtoNavigate);
     }
@@ -608,14 +670,19 @@ onUnmounted(() => {
       }
       &.heading-category {
         opacity: 1 !important;
-        transition: left 1s ease, opacity 1s ease !important;
         text-decoration: underline !important;
         text-decoration-thickness: 1px !important;
+        transition: opacity 1s ease !important;
         // z-index: 20;
+        pointer-events: all !important;
+        &.transitionleft {
+          transition: left 1s ease, opacity 1s ease !important;
+        }
       }
       &.hide {
         opacity: 0;
         transition: opacity 1s ease;
+        pointer-events: none;
         @include mobile {
           opacity: 1;
         }
