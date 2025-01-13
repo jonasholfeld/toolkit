c5c34c36e951865043a424a08b85852528e7340e
Jonas Holfeld <klatschpauke@googlemail.com>
2025-01-13 21:10:29 +0100

    favico

--- site/templates/_app-index.php
+++ site/templates/_app-index.php
@@ -24,23 +24,37 @@
 
   <link rel="manifest" href="/manifest.json">
 
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-<meta name="theme-color" content="#ffffff">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+
+  <link rel="apple-touch-icon" sizes="57x57" href="/light_apple-icon-57x57.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="60x60" href="/light_apple-icon-60x60.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="72x72" href="/light_apple-icon-72x72.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="76x76" href="/light_apple-icon-76x76.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="114x114" href="/light_apple-icon-114x114.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="120x120" href="/light_apple-icon-120x120.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="144x144" href="/light_apple-icon-144x144.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="152x152" href="/light_apple-icon-152x152.png" media="(prefers-color-scheme: light)">
+  <link rel="apple-touch-icon" sizes="180x180" href="/light_apple-icon-180x180.png" media="(prefers-color-scheme: light)">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/light_android-icon-192x192.png" media="(prefers-color-scheme: light)">
+  <link rel="icon" type="image/png" sizes="32x32" href="/light_favicon-32x32.png" media="(prefers-color-scheme: light)">
+  <link rel="icon" type="image/png" sizes="96x96" href="/light_favicon-96x96.png" media="(prefers-color-scheme: light)">
+  <link rel="icon" type="image/png" sizes="16x16" href="/light_favicon-16x16.png" media="(prefers-color-scheme: light)">
+
+  <link rel="apple-touch-icon" sizes="57x57" href="/dark_apple-icon-57x57.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="60x60" href="/dark_apple-icon-60x60.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="72x72" href="/dark_apple-icon-72x72.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="76x76" href="/dark_apple-icon-76x76.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="114x114" href="/dark_apple-icon-114x114.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="120x120" href="/dark_apple-icon-120x120.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="144x144" href="/dark_apple-icon-144x144.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="152x152" href="/dark_apple-icon-152x152.png" media="(prefers-color-scheme: dark)">
+  <link rel="apple-touch-icon" sizes="180x180" href="/dark_apple-icon-180x180.png" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/dark_android-icon-192x192.png" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" sizes="32x32" href="/dark_favicon-32x32.png" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" sizes="96x96" href="/dark_favicon-96x96.png" media="(prefers-color-scheme: dark)">
+  <link rel="icon" type="image/png" sizes="16x16" href="/dark_favicon-16x16.png" media="(prefers-color-scheme: dark)">
+
+  <link rel="manifest" href="/manifest.json">
 
   <?= vueKit()->preloadJson($page->uri()) ?>
   <?= vueKit()->preloadModule($page->intendedTemplate()->name()) ?>
